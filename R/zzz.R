7f35bb3888ba201081dbed8ebf559fc278b95500
Scott Brenstuhl <brenstsr@miamioh.edu>
2017-10-11 10:05:59 -0700

    First version of general api call

--- R/zzz.R
+++ R/zzz.R
@@ -37,6 +37,58 @@ NULL
     }
 }
 
+#' zendesk_get
+#' 
+#' This is a a general purpose function for any valid Zendesk API endpoint
+#' 
+#' This function will return a data.frame of the results returned from the
+#' zendesk API for the endpoint passed to it. Any valid parameters can be passed
+#' to refine the request, for some endpoints there are required parameters.
+#' 
+#' @param zd_call the api call (tickets, users, etc.)
+#' @param ... any valid parameters for the associated call
+#'
+#' @return returns a data.frame of the results from the given call
+#' @export
+#'
+#' @examples \dontrun{
+#' 
+#' ## This requires Zendesk authentication
+#' tickets <- zendesk_get('tickets')
+#' }
+zendesk_get <- function(zd_call, ...){
+    results <- list()
+    i <- 1
+    response <- httr::GET(paste0(.ZendeskEnv$data$url, '/api/v2/',
+                                 zd_call, '.json'),
+                         httr::authenticate(.ZendeskEnv$data$username,
+                                            .ZendeskEnv$data$password),
+                         query = list(...))
+    response_json <- httr::content(response, 'text')
+    results[[i]] <- jsonlite::fromJSON(response_json)[[zd_call]]
+    stop_paging <- is.null(httr::content(response)$next_page)
+    
+    while(!stop_paging) {
+        i <- i + 1
+        response <- httr::GET(httr::content(response)$next_page,
+                              httr::authenticate(.ZendeskEnv$data$username,
+                                                 .ZendeskEnv$data$password))
+        response_json <- httr::content(response, 'text')
+        results[[i]] <- jsonlite::fromJSON(response_json)[[zd_call]]
+        stop_paging <- is.null(httr::content(response)$next_page)
+    }
+    tidy_results(results)
+}
+
+tidy_results <- function(results_list){
+    output <- data.frame()
+    for(i in 1:length(results_list)){
+        output <- dplyr::bind_rows(output, 
+                                   jsonlite::flatten(results_list[[i]]))
+    }
+    output
+}
+
 #' unlistDataFrame
 #'
 #' Utility function to unlist the columns of each data.frame where necessary.
@@ -58,8 +110,7 @@ NULL
 #' ## This requires Zendesk authentication
 #' unlistDataFrame(dataframe)
 #' }
-
-unlistDataFrame <- 
+unlistDataFrame <-
 function(dataframe){
 	n <- dim(dataframe)[1]
 	dim.df <- dim(dataframe)[2]
@@ -70,7 +121,7 @@ function(dataframe){
 	for(i in 1:dim.df){
 		if(length(unlist(dataframe[,i])) == n){
 			dataframe[,i] <- unlist(dataframe[,i])
-		} 
+		}
 		if(length(unlist(dataframe[,i])) < n){
 			dataframe[,i] <- as.character(dataframe[,i])
 		}
@@ -79,4 +130,4 @@ function(dataframe){
 	return (dataframe)
 }
 
-      
+
